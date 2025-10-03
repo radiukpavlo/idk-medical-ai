39adc95d94b077362ef968cd283e584a6d62e669
Pavlo Radiuk <radiukpavlo@gmail.com>
2025-10-03 16:32:52 +0300

    dotnet run --project src/MedicalAI.UI: sucess

--- src/MedicalAI.Plugins/Segmentation.SKIFSeg/obj/Debug/net8.0/Segmentation.SKIFSeg.AssemblyInfo.cs
+++ src/MedicalAI.Plugins/Segmentation.SKIFSeg/obj/Debug/net8.0/Segmentation.SKIFSeg.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Segmentation.SKIFSeg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2d7b1e0299dbc627bde870cbffbf96dd432abf9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2feb190abf6fe53dd08206300e205729f9d39034")]
 [assembly: System.Reflection.AssemblyProductAttribute("Segmentation.SKIFSeg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Segmentation.SKIFSeg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
